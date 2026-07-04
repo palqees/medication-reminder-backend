269cc53d4d6e16e9cfc5ca60ff17a092bd55ad53
palqees <134081880+palqees@users.noreply.github.com>
2026-07-04 22:27:28 +0300

    fix auth controller

--- Backend.A/src/config/db.js
+++ Backend.A/src/config/db.js
@@ -25,3 +25,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
--- Backend.A/src/controllers/authController.js
+++ Backend.A/src/controllers/authController.js
@@ -1,37 +1,78 @@
 // src/controllers/authController.js
+
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+// Register
 exports.register = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({ name, email, password: hashedPassword });
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  res.status(201).json({ message: "User registered", user });
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user
+    });
+
+  } catch (error) {
+
+    // duplicate email
+    if (error.code === 11000) {
+      return res.status(400).json({
+        message: "Email already exists"
+      });
+    }
+
+    res.status(500).json({
+      message: error.message
+    });
+  }
 };
 
+// Login
 exports.login = async (req, res) => {
-  const { email, password } = req.body;
+  try {
 
-  const user = await User.findOne({ email });
-  if (!user) return res.status(400).json({ message: "Invalid credentials" });
+    const { email, password } = req.body;
 
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+    const user = await User.findOne({ email });
 
-  const token = jwt.sign(
-    { id: user._id },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials"
+      });
+    }
 
-  res.json({ token });
-  if (error.code === 11000) {
-  return res.status(400).json({
-    message: "Email already exists"
-  });
-}
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
 
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({ token });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
 };
