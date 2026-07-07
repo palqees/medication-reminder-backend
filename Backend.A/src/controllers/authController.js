441508e83f2563c884e6820fd080949547ed176d
palqees <134081880+palqees@users.noreply.github.com>
2026-07-07 23:35:54 +0300

    return user data token in logen

--- Backend.A/src/controllers/authController.js
+++ Backend.A/src/controllers/authController.js
@@ -68,7 +68,14 @@ exports.login = async (req, res) => {
       { expiresIn: "7d" }
     );
 
-    res.json({ token });
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
 
   } catch (error) {
     res.status(500).json({
