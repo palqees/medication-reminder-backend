2cb9cc236cdd50bd5f07d9fcf1c052fc424dbf9c
palqees <134081880+palqees@users.noreply.github.com>
2026-07-07 22:08:54 +0300

    enable cors

--- Backend.A/src/app.js
+++ Backend.A/src/app.js
@@ -1,20 +1,36 @@
 // src/app.js
+// const express = require("express");
+
+// const authRoutes = require("./routes/authRoutes");
+// const medicineRoutes = require("./routes/medicineRoutes");
+
+// const app = express();
+
+// app.use(express.json());
+
+
+// // Routes
+// app.use("/api/auth", authRoutes);
+
+// app.use("/api/medicines", medicineRoutes);
+
+
+// module.exports = app;
+// const cors = require("cors");
+// app.use(cors());
+
 const express = require("express");
+const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
 const medicineRoutes = require("./routes/medicineRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-
-// Routes
 app.use("/api/auth", authRoutes);
-
 app.use("/api/medicines", medicineRoutes);
 
-
 module.exports = app;
-const cors = require("cors");
-app.use(cors());
